b07df19b7e367d9ca1b2f924aa2614c80400e43e
Henrique Adhmann Pires <henrique.pires@centercell.br>
2023-10-30 10:05:18 -0300

    feat: add querys

--- front/src/hooks/firebase.js
+++ front/src/hooks/firebase.js
@@ -25,16 +25,28 @@ const firebaseApp = initializeApp(firebaseConfig)
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
+const users = collection(db, "users");
 const stock = collection(db, "stock");
-//const users = collection(db, 'users');
+const logStock = collection(db, "logStock");
+const requisicoes = collection(db, "requisicoes");
+const itensRequisitados = collection(db, "itensRequisitados");
+const logRequisicoes = collection(db, "logRequisicoes");
 
-export const signUp = async (email, pass) => {
+export const signUp = async (email, pass, name='', origin='') => {
     if (!getUid) return 'Usuário não permitido.';
     try {
         const result = await createUserWithEmailAndPassword(auth, email, pass);
+        const user = {
+            ativo: true,
+            email,
+            name,
+            origin,
+            uid: result.user.uid,
+        };
+        await addDoc(users, user);
         return result;
     } catch (err) {
-        return err
+        return err;
     }
 }
 
@@ -57,13 +69,28 @@ export const logOutUser = () => {
 
 export const getUid = async () => {
     const user = auth.currentUser;
-    return user ? true : false;
+    return user ? user : false;
 }
 
 export const insertStock = async (obj) => {
-    if (!(await getUid())) return 'Usuário não permitido.';
-    await addDoc(stock, obj);
-    return 'Item adicionado.';
+    const date = new Date();
+    const user = await getUid();
+    if (!(user)) return 'Usuário não permitido.';
+    try {
+        const item = await addDoc(stock, obj);
+        const log = {
+            alteracao: 'New',
+            dtAlteracao: date.toLocaleString(),
+            idItem: item.id,
+            idUser: user.uid,
+            qtd: obj.amount,
+        };
+        await addDoc(logStock, log);
+        return 'Item adicionado.';
+    } catch (e) {
+        console.log(e);
+        return 'Erro ao adicionar item';
+    }
 }
 
 export const selectItemStock = async (id) => {
@@ -78,15 +105,83 @@ export const selectAllStock = async () => {
 }
 
 export const updateStock = async (id, obj) => {
-    if (!(await getUid())) return 'Usuário não permitido.';
-    const docItem = doc(db, 'stock', id);
-    await updateDoc(docItem, obj)
-    return 'Quantidade atualizada.';
+    const user = await getUid();
+    const date = new Date();
+    if (!(user)) return 'Usuário não permitido.';
+    try {
+        const docItem = doc(db, 'stock', id);
+        await updateDoc(docItem, obj)
+        const log = {
+            alteracao: 'Update',
+            dtAlteracao: date.toLocaleString(),
+            idItem: id,
+            idUser: user.uid,
+            qtd: obj.amount,
+        }
+        await addDoc(logStock, log);
+        return 'Quantidade atualizada.';
+    } catch (e) {
+        console.log(e);
+        return 'Erro ao atualizar quantidade.';
+    }
 }
 
 export const deleteStock = async (id) => {
-    if (!(await getUid())) return 'Usuário não permitido.';
-    const docItem = doc(db, 'stock', id);
-    await deleteDoc(docItem)
-    return 'Item removido.';
+    const user = await getUid();
+    const date = new Date();
+    if (!(user)) return 'Usuário não permitido.';
+    try {
+        const docItem = doc(db, 'stock', id);
+        await deleteDoc(docItem)
+        const log = {
+            alteracao: 'Delete',
+            dtAlteracao: date.toLocaleString(),
+            idItem: id,
+            idUser: user.uid,
+            qtd: '0',
+        }
+        await addDoc(logStock, log);
+        return 'Item removido.';
+    } catch(e) {
+        console.log(e);
+        return 'Erro ao remover item.';
+    }
+}
+
+export const insertRequisicao = async (obj) => {
+    const date = new Date();
+    const user = await getUid();
+    if (!(user)) return 'Usuário não permitido.';
+    try {
+        const newObj = {
+            ...obj,
+            dtRequisicao: date.toLocaleString(),
+            user: user.uid,
+        }
+        await addDoc(requisicoes, newObj);
+        return 'Requisicao adicionada.';
+    } catch (e) {
+        console.log(e);
+        return 'Erro ao adicionar requisicao';
+    }
+}
+
+export const updateRequisicao = async (id, obj) => {
+    const date = new Date();
+    const user = await getUid();
+    if (!(user)) return 'Usuário não permitido.';
+    try {
+        const docItem = doc(db, 'requisicoes', id);
+        await updateDoc(docItem, obj)
+        const log = {
+            dtAnalisado: date.toLocaleString(),
+            idRequisicao: id,
+            idUser: user.uid,
+        }
+        await addDoc(logStock, log);
+        return 'Requisicao atendida.';
+    } catch (e) {
+        console.log(e);
+        return 'Erro ao atender requisicao.';
+    }
 }
